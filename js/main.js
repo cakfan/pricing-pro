e5c02aa0d1ab42d588bc185cdbbac73a441077ea
cakfan <20588976+cakfan@users.noreply.github.com>
2025-04-17 09:07:23 +0000

    Deploying to gh-pages from @ cakfan/pricing-pro@bd5e44e8836a8173f77d301829b783c330210b53 🚀

--- js/main.js
+++ js/main.js
@@ -7,11 +7,23 @@ document
 
     // Inisialisasi state awal
     if (index === 0) {
+      accordion.classList.add("pt-6");
+      accordion.classList.add("pr-6");
+      accordion.classList.add("pb-6");
+      accordion.classList.remove("pt-[19.03px]");
+      accordion.classList.remove("pr-[19.03px]");
+      accordion.classList.remove("pb-[19.03px]");
       panel.style.marginTop = "12px";
       panel.style.maxHeight = panel.scrollHeight + "px"; // Buka accordion pertama
       icon.src = "./assets/images/icons/cross-x-filled.svg"; // Ikon terbuka
       icon.classList.remove("-rotate-45");
     } else {
+      accordion.classList.add("pt-[19.03px]");
+      accordion.classList.add("pr-[19.03px]");
+      accordion.classList.add("pb-[19.03px]");
+      accordion.classList.remove("pt-6");
+      accordion.classList.remove("pr-6");
+      accordion.classList.remove("pb-6");
       panel.style.marginTop = "0px";
       panel.style.maxHeight = "0px"; // Tutup accordion lainnya
       icon.src = "./assets/images/icons/cross-x.svg"; // Ikon tertutup
@@ -27,11 +39,23 @@ document
         const ic = acc.querySelector("img");
 
         if (acc === accordion && !isCurrentlyOpen) {
+          acc.classList.add("pt-6");
+          acc.classList.add("pr-6");
+          acc.classList.add("pb-6");
+          acc.classList.remove("pt-[19.03px]");
+          acc.classList.remove("pr-[19.03px]");
+          acc.classList.remove("pb-[19.03px]");
           p.style.marginTop = "12px";
           p.style.maxHeight = p.scrollHeight + "px"; // Buka accordion
           ic.src = "./assets/images/icons/cross-x-filled.svg"; // Ganti ikon ke terbuka
           ic.classList.remove("-rotate-45");
         } else {
+          acc.classList.remove("pt-6");
+          acc.classList.remove("pr-6");
+          acc.classList.remove("pb-6");
+          acc.classList.add("pt-[19.03px]");
+          acc.classList.add("pr-[19.03px]");
+          acc.classList.add("pb-[19.03px]");
           p.style.marginTop = "0px";
           p.style.maxHeight = "0px"; // Tutup accordion
           ic.src = "./assets/images/icons/cross-x.svg"; // Ganti ikon ke tertutup
